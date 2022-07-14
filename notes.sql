af8223d2f66df3f9fe025f8e1e99d30c5e461082
Scott Simpson <scott@scottsimpson.net>
2022-07-14 23:37:08 +0000

    Updated Exercise Files

---
+++ notes.sql
@@ -0,0 +1,8 @@
+-- This file is intended for notes
+-- and for demonstrating how to work
+-- with SQLite in Codespaces.
+
+-- These two statements operate on the 
+-- restaurant.db SQLite database.
+SELECT * FROM Dishes;
+SELECT * FROM Customers;
