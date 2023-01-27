20d313b43ad25b4ffcf41fb7c45fb84a85abc96a
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2023-01-27 13:28:55 -1000

    Restaurant
    in the reataurant

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -5,3 +5,7 @@
 -- they plan to bring in their party.
 
 -- Hint: SQLite offers the INTEGER and REAL datatypes
+
+CREATE TABLE AnniversaryAttendees (
+CustomerID INTEGER,
+PartySize INTEGER );
--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,19 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT * 
+FROM Dishes
+ORDER BY Price;
+
+SELECT Type, Name, Price, Description
+FROM Dishes
+WHERE Type = 'Appetizer' OR Type = 'Beverage'
+ORDER BY Type;
+
+SELECT Type, Name, Price, Description
+FROM Dishes
+WHERE Type != 'Beverage'
+ORDER BY Type;
+
+
