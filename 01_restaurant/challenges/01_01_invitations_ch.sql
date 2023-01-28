4e0f447d930e2053df06ae95f5ccb041f421619d
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2023-01-27 14:00:39 -1000

    Reservations
    Reservations

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -3,3 +3,6 @@
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
 
+SELECT FirstName, LastName, Email 
+FROM Customers 
+ORDER BY LastName;
--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,19 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+FROM Customers
+WHERE FirstName = 'Norby' OR LastName = 'Norby';
+
+SELECT *
+FROM Reservations
+WHERE CustomerID = 64 AND Reservations.Date > '2022-07-24';
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
+
+-- or:
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE (FirstName = 'Norby' OR LastName = 'Norby')
+AND Reservations.Date > '2022-07-24';
