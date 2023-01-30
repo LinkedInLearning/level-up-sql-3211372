2a1c16c1890a1ace9c940c96eaf5b14e7fd16473
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2023-01-29 16:31:30 -1000

    Createreservation
    Create a reservation

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,25 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+
+SELECT * 
+FROM Customers 
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Customers 
+(FirstName, LastName, Email, Phone) VALUES 
+('Sam', 'McAdams', 'smac@kinetecoinc.com', '(555) 555-1232');
+
+SELECT * 
+FROM Customers 
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Reservations 
+(CustomerID, Date, PartySize) VALUES 
+(102, '2022-08-12 18:00:00', 5);
+
+SELECT Customers.FirstName, Customers.LastName, Customers.Email, Reservations.Date, Reservations.PartySize 
+FROM Customers 
+JOIN Reservations ON Customers.CustomerID = Reservations.CustomerID 
+WHERE Customers.Email = 'smac@kinetecoinc.com';
