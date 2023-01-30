be75fd30f51ffcbfead0ebf941e0a5ace5067fee
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2023-01-29 17:41:49 -1000

    Dishes.Price
    customer's delivery order

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -5,24 +5,21 @@
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
 
-
 SELECT * 
-FROM Customers 
+FROM Customers
 WHERE Email = 'smac@kinetecoinc.com';
 
-INSERT INTO Customers 
-(FirstName, LastName, Email, Phone) VALUES 
-('Sam', 'McAdams', 'smac@kinetecoinc.com', '(555) 555-1232');
+INSERT INTO Customers ( FirstName, LastName, Email)
+VALUES ( 'Sam', 'McAdams', 'smac@kinetecoinc.com');
 
-SELECT * 
-FROM Customers 
+INSERT INTO Reservations ( CustomerID, Date, PartySize)
+VALUES ( 102, '2022-08-12 18:00:00', '5');
+
+SELECT *
+FROM Reservations
+WHERE CustomerID = 102;
+
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
 WHERE Email = 'smac@kinetecoinc.com';
-
-INSERT INTO Reservations 
-(CustomerID, Date, PartySize) VALUES 
-(102, '2022-08-12 18:00:00', 5);
-
-SELECT Customers.FirstName, Customers.LastName, Customers.Email, Reservations.Date, Reservations.PartySize 
-FROM Customers 
-JOIN Reservations ON Customers.CustomerID = Reservations.CustomerID 
-WHERE Customers.Email = 'smac@kinetecoinc.com';
--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,31 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT *
+FROM Customers
+WHERE LastName = 'Hundey';
+
+INSERT INTO Orders ( CustomerID, OrderDate)
+VALUES ( 70, '2022-09-20 14.00.00');
+
+SELECT Customers.LastName, Customers.LastName, Orders.OrderID, Orders.OrderDate
+FROM Orders
+JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+WHERE Email = 'lhundey1x@kinetecoinc.com'
+ORDER BY OrderID DESC;
+
+INSERT INTO OrdersDishes ( OrderID, DishID )
+VALUES ( 1001, ( SELECT DishID FROM Dishes WHERE Name = 'House Salad')),
+( 1001, ( SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers')),
+( 1001, ( SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+SELECT *
+FROM Dishes
+JOIN OrdersDishes ON OrdersDishes.DishID = Dishes.DishID
+WHERE OrderID = 1001;
+
+SELECT SUM(Dishes.Price) 
+FROM Dishes 
+JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID 
+WHERE OrdersDishes.OrderID = 1001;
