87d24599ffc8a8016e5b37cc611cb203c28f0769
KekgethilweKey <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-12-04 08:11:05 +0000

    Modify 01_restaurant/.../01_01_invitations_ch. Create list of customers for invitations.

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,15 @@
--- Generate a list of customer information.
-
+/*
+CREATED BY: Kekgethilwe
+CREATE DATE: 04/12/2023
+DESCRIPTION: Generate a list of customer information.
+*/
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
+SELECT 
+ FirstName,
+ LastName,
+ Email
+FROM 
+ Customers
+ORDER BY
+ LastName;
