1e4a609fdc23acff44d49699f97fa421a0836037
dshendler <dshendler@gmail.com>
2023-12-26 22:04:01 +0000

    Just messing around, practicing Commits

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,4 @@
--- Generate a list of customer information.
+-- Generate a list -- yes a list!--of customer information.
 
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
