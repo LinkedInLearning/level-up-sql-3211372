b3c4ede5a58ae124d13a727c7cbd0383e9a2bbc9
dshendler <dshendler@gmail.com>
2023-12-28 16:55:05 +0000

    Saving my halfway point. First 4 of Library done.

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,43 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT
+  BookID
+FROM 
+  Books
+WHERE
+  Title = 'Dracula';
+-- IDs are 12, 60, 73
+
+-- ---------- ** First trial -----------
+SELECT *
+FROM 
+  Loans
+WHERE
+  BookID IN (12, 60, 73)
+ORDER BY 
+  BookID
+  ;
+-- ------- ** My completed steps ----------
+SELECT
+  LoanID,
+  b.BookID,
+  b.Title,
+  b.Author,
+  PatronID,
+  MAX(LoanDate) [Loan Date],
+  DueDate,
+  ReturnedDate
+FROM
+  Loans l
+INNER JOIN
+  Books b
+ON
+  b.BookID = l.BookID
+WHERE
+  l.BookID IN (SELECT BookID FROM Books WHERE Title = 'The Scarlet Letter')
+GROUP BY 
+  l.BookID;
+
+
+
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,35 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+-- First, the simple code --------
+SELECT
+  BookID,
+  Title
+FROM
+  Books
+WHERE
+  Barcode = 2855934983 OR
+  Barcode = 4043822646;
+
+SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com';
+
+-- ---------------- ** My integrated code ------------------
+INSERT INTO
+  Loans (BookID, PatronID, LoanDate, DueDate)
+  VALUES (
+    (SELECT BookID FROM Books WHERE Barcode = 2855934983),
+    (SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com'),
+    '2022-08-25', '2022-09-08'
+   ),
+   ( 
+    (SELECT BookID FROM Books WHERE Barcode = 4043822646),
+    (SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com'),
+    '2022-08-25', '2022-09-08'
+  )
+
+-- -------------- Code for checking -----------------
+SELECT *
+FROM Loans
+ORDER BY LoanID DESC
+LIMIT 5; 
